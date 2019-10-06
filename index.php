605f669b6b00796a72733a5f5c7789db964cab96
Roman Golovko <senior.golovko-r@yandex.ru>
2019-10-06 13:18:20 +0300

    2.1

--- index.php
+++ index.php
@@ -1,5 +1,80 @@
 <?<?php 
+$a=-10;
+$b=-12;
+echo "Первое задаени".PHP_EOL;
+if ($a>=0&&$b>=0) echo $a-$b;
+elseif ($a<0&&$b<0) {
+	# code...
+echo $a*$b;
+}
+else echo $a+$b;
 
+echo PHP_EOL."Второе задание".PHP_EOL;
+$a=7;
+switch ($a) {
+	case $a<=1:
+		# code...
+		echo $a.PHP_EOL;
+		$a++;
+	case $a<=2:
+		# code...
+		echo $a.PHP_EOL;
+		$a++;
+	case $a<=3:
+		# code...
+		echo $a.PHP_EOL;
+		$a++;
+	case $a<=4:
+		# code...
+		echo $a.PHP_EOL;
+		$a++;
+	case $a<=5:
+		# code...
+		echo $a.PHP_EOL;
+		$a++;
+	case $a<=6:
+		# code...
+		echo $a.PHP_EOL;
+		$a++;
+	case $a<=7:
+		# code...
+		echo $a.PHP_EOL;
+		$a++;
+	case $a<=8:
+		# code...
+		echo $a.PHP_EOL;
+		$a++;
+	case $a<=9:
+		# code...
+		echo $a.PHP_EOL;
+		$a++;
+	case $a<=10:
+		# code...
+		echo $a.PHP_EOL;
+		$a++;
+	case $a<=11:
+		# code...
+		echo $a.PHP_EOL;
+		$a++;
+	case $a<=12:
+		# code...
+		echo $a.PHP_EOL;	
+		$a++;
+    case $a<=13:
+		# code...
+		echo $a.PHP_EOL;
+		$a++;
+	case $a<=14:
+		# code...
+		echo $a.PHP_EOL;
+		$a++;
+	case $a<=15:
+		# code...
+		echo $a.PHP_EOL;		
+		$a++;
+		}
+
+echo PHP_EOL."Третье задание";
  ?>
 
  <!DOCTYPE html>
@@ -7,6 +82,7 @@
  <head>
  	<meta charset="utf-8">
  	<title>Вторая домашка</title>
+ 	
  </head>
  <body>
  
